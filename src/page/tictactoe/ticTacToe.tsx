d5bb652e2ad88efe787ee8e6a4c3052d8279188a
jhlee <dove92lee@gmail.com>
2023-10-04 17:38:01 +0900

    틱택토 50%완성

--- src/page/tictactoe/ticTacToe.tsx
+++ src/page/tictactoe/ticTacToe.tsx
@@ -1,31 +1,49 @@
 import React, { useState } from 'react';
 
-function Square(value) {
-  return <>
-    <button>
-      value
+function Square({value, onSquareClick }) {
+  return (
+    <button
+      style={{
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      onClick = { () => onSquareClick()}
+    >
+      <div>{value}</div>
     </button>
-  </>
+  );
 }
 
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    console.log(nextSquares, '이전값')
+    nextSquares[i] = "X"
+    console.log(nextSquares,'이후값')
+    setSquares(nextSquares)
+  }
   return (
-    <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+    <div style={{marginTop:'200px', marginLeft: '200px'}}>
+      <div className="board-row" style={{ display: 'flex' }}>
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
       </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+      <div className="board-row" style={{ display: 'flex' }}>
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
       </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+      <div className="board-row" style={{ display: 'flex' }}>
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
       </div>
-    </>
+    </div>
   );
 }
