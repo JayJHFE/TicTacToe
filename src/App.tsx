cdbd72cc6998b362631cebe3b10962027578e281
jhlee <dove92lee@gmail.com>
2023-10-04 12:23:52 +0900

    틱택토 기본설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from'react'
 import './App.css';
+const Logo = require('../src/Logo.svg');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
